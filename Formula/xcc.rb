780e577ff62bad1a3195d5c6b2608d959ee70290
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-11 22:08:34 +0000

    Add xcc

---
+++ Formula/xcc.rb
@@ -0,0 +1,11 @@
+class Xcc < Formula
+  desc ""
+  homepage "https://github.com/Finnvoor/xcc"
+  url "https://github.com/Finnvoor/xcc/releases/download/1.0.0/1.0.0.tar.gz"
+  sha256 "419550dcb8d5b1dd60d51a7a59227a0ea36a1eaa5b646d8c9d15b91fbd620da5"
+  license "CC0-1.0"
+  
+  def install
+    bin.install "xcc"
+  end
+end
