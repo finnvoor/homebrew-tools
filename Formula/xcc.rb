f947f72bc49011ed6faad22d9abafca789e84767
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-12 00:02:54 +0000

    xcc 1.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/xcc.rb
+++ Formula/xcc.rb
@@ -1,8 +1,8 @@
 class Xcc < Formula
   desc ""
   homepage "https://github.com/Finnvoor/xcc"
-  url "https://github.com/Finnvoor/xcc/releases/download/1.0.0/1.0.0.tar.gz"
-  sha256 "419550dcb8d5b1dd60d51a7a59227a0ea36a1eaa5b646d8c9d15b91fbd620da5"
+  url "https://github.com/finnvoor/xcc/releases/download/1.0.1/1.0.1.tar.gz"
+  sha256 "19c7dbcf35ab6047616038696ae415663df71fd0fcc1ef18e032b7a2fe9336e8"
   license "CC0-1.0"
   
   def install
