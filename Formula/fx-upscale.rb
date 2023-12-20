9697f7a59f604fd73725caa759a3c4d19b5cd6cf
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-20 18:11:45 -0500

    Add fx-upscale

---
+++ Formula/fx-upscale.rb
@@ -0,0 +1,11 @@
+class FxUpscale < Formula
+  desc ""
+  homepage "https://github.com/Finnvoor/fx-upscale"
+  url "https://github.com/Finnvoor/fx-upscale/releases/download/1.0.2/1.0.2.tar.gz"
+  sha256 "2b47f0517c7e8a9ecaecf39370fccb6a033b09a8e16594e725580a7b8fe0f3cd"
+  license "CC0-1.0"
+  
+  def install
+    bin.install "fx-upscale"
+  end
+end
