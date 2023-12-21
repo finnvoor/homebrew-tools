b07fd3a1f27ba2c0c917aae56cbe41464e75c019
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-21 14:11:41 -0500

    xcc 1.0.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/xcc.rb
+++ Formula/xcc.rb
@@ -1,8 +1,8 @@
 class Xcc < Formula
   desc "CLI for Xcode Cloud"
   homepage "https://github.com/Finnvoor/xcc"
-  url "https://github.com/finnvoor/xcc/releases/download/1.0.4/1.0.4.tar.gz"
-  sha256 "e58af429cf39929f55b91c33e9f3b7595f111cb02789236c790a9e342d82034c"
+  url "https://github.com/finnvoor/xcc/releases/download/1.0.5/1.0.5.tar.gz"
+  sha256 "5f31e47d098373d6d1eeeeb121ca66db4ca93d4c057e1c93280f1b560299193d"
   license "CC0-1.0"
   
   def install
