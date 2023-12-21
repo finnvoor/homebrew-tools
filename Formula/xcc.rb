e9acdd63c9dce21ddeb14b2cff8affe660209dfc
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-21 14:09:30 -0500

    Fix errors

--- Formula/xcc.rb
+++ Formula/xcc.rb
@@ -1,5 +1,5 @@
 class Xcc < Formula
-  desc "A CLI for Xcode Cloud"
+  desc "CLI for Xcode Cloud"
   homepage "https://github.com/Finnvoor/xcc"
   url "https://github.com/finnvoor/xcc/releases/download/1.0.4/1.0.4.tar.gz"
   sha256 "e58af429cf39929f55b91c33e9f3b7595f111cb02789236c790a9e342d82034c"
