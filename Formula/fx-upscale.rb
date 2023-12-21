bdb4e643140a3ff9b7d9ab0845a593c0872749e7
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-21 14:14:08 -0500

    Fix errors

--- Formula/fx-upscale.rb
+++ Formula/fx-upscale.rb
@@ -4,7 +4,7 @@ class FxUpscale < Formula
   url "https://github.com/finnvoor/fx-upscale/releases/download/1.0.5/1.0.5.tar.gz"
   sha256 "bdd6bb2c14ee328cde751f59aaceb4bc8145874fb76c4c77640bdd7c034ead1e"
   license "CC0-1.0"
-  
+
   def install
     bin.install "fx-upscale"
   end
--- Formula/xcc.rb
+++ Formula/xcc.rb
@@ -4,7 +4,7 @@ class Xcc < Formula
   url "https://github.com/finnvoor/xcc/releases/download/1.0.5/1.0.5.tar.gz"
   sha256 "5f31e47d098373d6d1eeeeb121ca66db4ca93d4c057e1c93280f1b560299193d"
   license "CC0-1.0"
-  
+
   def install
     bin.install "xcc"
   end
