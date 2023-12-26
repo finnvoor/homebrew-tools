52cafb5937286f6e4c5d1007a1b7d1d06dace3bf
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-25 19:04:52 -0500

    fx-upscale 1.0.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/fx-upscale.rb
+++ Formula/fx-upscale.rb
@@ -1,8 +1,8 @@
 class FxUpscale < Formula
   desc "Metal-powered video upscaling"
   homepage "https://github.com/Finnvoor/fx-upscale"
-  url "https://github.com/finnvoor/fx-upscale/releases/download/1.0.6/1.0.6.tar.gz"
-  sha256 "67532772103bd762d3a322f455ac821aa2909c537e74f28f615c12d36f29b230"
+  url "https://github.com/finnvoor/fx-upscale/releases/download/1.0.7/1.0.7.tar.gz"
+  sha256 "e99881d6c9003736e1c48d3a29d425e8d1eb6e0631046c5ddb3b734af1669f3d"
   license "CC0-1.0"
 
   def install
