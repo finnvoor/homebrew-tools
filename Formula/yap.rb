e4d79498caa3d32f859c5fa9e2ddcab9c3c47976
Finn Voorhees <finnvoorhees@gmail.com>
2025-06-18 17:05:22 +0100

    Update yap.rb

--- Formula/yap.rb
+++ Formula/yap.rb
@@ -1,5 +1,5 @@
 class Yap < Formula
-  desc "A CLI for on-device speech transcription using Speech.framework"
+  desc "CLI for on-device speech transcription using Speech.framework"
   homepage "https://github.com/Finnvoor/yap"
   url "https://github.com/finnvoor/yap/releases/download/1.0.2/yap-1.0.2.tar.gz"
   sha256 "78e51334712fb5a6c393d682a7675f92b6f6215ca42558803362e68302769f91"
