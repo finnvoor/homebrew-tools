cf62e607673dcc7738b68de659c8e26f287483a6
Finn Voorhees <finnvoorhees@gmail.com>
2025-06-18 16:45:50 +0100

    Update yap.rb

--- Formula/yap.rb
+++ Formula/yap.rb
@@ -4,6 +4,7 @@ class Yap < Formula
   url "https://github.com/finnvoor/yap/releases/download/1.0.0/1.0.0.tar.gz"
   sha256 "68939e9c983626a1323fbaa1f3c64f76005d78c24d7f083e8b1412bae5d2af61"
   license "CC0-1.0"
+  depends_on macos: :tahoe
 
   def install
     bin.install "yap"
