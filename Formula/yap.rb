7e3bc466dbab6841d0798d5ff20d8cde4016067d
Finn Voorhees <finnvoorhees@gmail.com>
2026-02-10 11:20:09 +0000

    yap 1.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/yap.rb
+++ Formula/yap.rb
@@ -1,8 +1,8 @@
 class Yap < Formula
   desc "CLI for on-device speech transcription using Speech.framework"
   homepage "https://github.com/Finnvoor/yap"
-  url "https://github.com/finnvoor/yap/releases/download/1.0.6/yap-1.0.6.tar.gz"
-  sha256 "c2def0752e2a934b6d2d833bf037997c6149b1c1a80a730e3e0232db398b6a11"
+  url "https://github.com/finnvoor/yap/releases/download/1.1.0/yap-1.1.0.tar.gz"
+  sha256 "d5d09cfa56df13268159d569ecc53ad8a0be0fab26bf6451565ca2a3dd2b30ab"
   license "CC0-1.0"
   depends_on :macos
   depends_on macos: :tahoe
